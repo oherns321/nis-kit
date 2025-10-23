16ae51818611870c9b2496e9413118a05038c66d
oherns321 <sean.ohern@ey.com>
2025-10-23 09:00:56 -0400

    changed login section

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -131,47 +131,7 @@ export default async function decorate(block) {
         <div class="form-headline p-b-0">
           <h1 class="form-headline__title">Manage Your Account</h1>
         </div>
-        <form id="banner-login" action="#" method="post">
-          <div class="form-field">
-            <label for="username">
-              Email
-              <input type="email" name="username" id="username" placeholder="Email" class="w-100">
-            </label>
-            <a href="#" class="form-field__help-link form-field__help-link--top-right">Forgot email?</a>
-          </div>
-          <div class="form-field form-field--password">
-            <label for="password">
-              Password
-              <span class="relative">
-                <input type="password" name="password" id="password" placeholder="Password" class="w-100">
-                <button title="Show Password" type="button" class="show-password">
-                  <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
-                  </svg>
-                </button>
-              </span>
-            </label>
-            <a href="#" class="form-field__help-link form-field__help-link--top-right">Forgot password?</a>
-          </div>
-          <div class="form-field">
-            <label for="rememberme">
-              <input type="checkbox" name="rememberme" id="rememberme" value="true" checked>
-              <span class="text-400 color-blue">Remember my email</span>
-            </label>
-          </div>
-          <div class="form-field">
-            <input type="submit" value="Sign In" class="btn btn-primary w-100 m-t-0">
-          </div>
-          <div class="form-field text-sm text-center">
-            <br>
-            <div class="text-medium color-grey m0">Don't have an online account?</div>
-            <div class="text-sm">
-              <a href="#" class="color-blue">Register an account</a>
-              or
-              <a href="#" class="color-blue">Pay without signing in</a>
-            </div>
-          </div>
-        </form>
+        <div data-vue-app="logged-in-user"><!----> <div><div class="login-banner__welcome"><div class="test m-t-30 m-b-100"><p class="text-700 text-sm m-b-0 m-t-10">Email</p> <p>Tyler Schleich <a href="https://myaccount.columbiagasohio.com/logout" class="text-xs text-300 text-gray m-l-md-5"> Sign Out</a></p> <a href="https://myaccount.columbiagasohio.com/dashboard" class="btn btn--primary m-t-10 m-b-10">Access My Account</a></div></div></div></div>
       </div>
     </div>
   `;
@@ -180,26 +140,4 @@ export default async function decorate(block) {
   // Add both banners to the block
   block.appendChild(mobileBanner);
   block.appendChild(desktopBanner);
-
-  // Add password show/hide functionality
-  const showPasswordBtn = block.querySelector('.show-password');
-  const passwordInput = block.querySelector('#password');
-
-  if (showPasswordBtn && passwordInput) {
-    showPasswordBtn.addEventListener('click', function passwordToggle(e) {
-      e.preventDefault();
-      const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-      passwordInput.setAttribute('type', type);
-
-      // Update icon (optional - you could swap the SVG here)
-      const icon = this.querySelector('svg path');
-      if (type === 'text') {
-        // Show "hide" icon
-        icon.setAttribute('d', 'M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92 1.41-1.41L3.51 2.3 2.1 3.71l2.8 2.8C3.9 8.26 2.73 9.89 2 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l2.92 2.92 1.41-1.41L12 7z');
-      } else {
-        // Show "show" icon
-        icon.setAttribute('d', 'M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z');
-      }
-    });
-  }
 }
